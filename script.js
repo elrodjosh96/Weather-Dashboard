ec4b12b9724c83b26ccb8a36edada681bec6b216
Josh Elrod <elrodjosh96@gmail.com>
2021-10-05 19:33:52 -0400

    updated JS, partial data printed in cards

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var cityWeather = 'Chicago'
+var cityWeather = 'Atlanta'
 var weathAPI = 'https://api.openweathermap.org/data/2.5/weather?q='+ cityWeather + '&appid=a55cbfd2e3c5718ac038ceb575faa02d'
 fetch(weathAPI) 
     .then(function (response) {
@@ -12,7 +12,6 @@ document.getElementById('search-btn').addEventListener('click', function(event){
     cityWeather = document.getElementById('citysearch').value
     console.log(cityWeather) 
     APIcall(cityWeather);
-
 })
 
 function APIcall(cityWeather) {
@@ -33,9 +32,82 @@ fetch(weathAPI)
             .then(function (data) {
                 console.log(data)
             })
-
+            Day1Data(data.name, data.main.temp, data.main.humidity, data.wind.speed);
+            Day2Data(data.name, data.main.temp, data.main.humidity, data.wind.speed);
+            Day3Data(data.name, data.main.temp, data.main.humidity, data.wind.speed);
+            Day4Data(data.name, data.main.temp, data.main.humidity, data.wind.speed);
+            Day5Data(data.name, data.main.temp, data.main.humidity, data.wind.speed);
       });
 }
 
+function Day1Data(name, temp, humidity, speed) {
+    $(".card-title").text(name);
+    // $("section").attr("style", "display:inline");
+    var tempF = Math.round((temp - 273.15) * 1.8 + 32);
+    $("#temp1").text('Temperature: ' + tempF + "°");
+    var wind = Math.round(speed);
+    $("#wind1").text('Wind: ' + wind + "mph");
+    $("#humidity1").text('Humidity: ' + humidity + "%");
+  }
+
+  function Day2Data(name, temp, humidity, speed) {
+    $(".card-title").text(name);
+    // $("section").attr("style", "display:inline");
+    var tempF = Math.round((temp - 273.15) * 1.8 + 32);
+    $("#temp2").text('Temperature: ' + tempF + "°");
+    var wind = Math.round(speed);
+    $("#wind2").text('Wind: ' + wind + "mph");
+    $("#humidity2").text('Humidity: ' + humidity + "%");
+  }
+  function Day3Data(name, temp, humidity, speed) {
+    $(".card-title").text(name);
+    // $("section").attr("style", "display:inline");
+    var tempF = Math.round((temp - 273.15) * 1.8 + 32);
+    $("#temp2").text('Temperature: ' + tempF + "°");
+    var wind = Math.round(speed);
+    $("#wind2").text('Wind: ' + wind + "mph");
+    $("#humidity2").text('Humidity: ' + humidity + "%");
+  }
+  function Day4Data(name, temp, humidity, speed) {
+    $(".card-title").text(name);
+    // $("section").attr("style", "display:inline");
+    var tempF = Math.round((temp - 273.15) * 1.8 + 32);
+    $("#temp2").text('Temperature: ' + tempF + "°");
+    var wind = Math.round(speed);
+    $("#wind2").text('Wind: ' + wind + "mph");
+    $("#humidity2").text('Humidity: ' + humidity + "%");
+  }
+  function Day5Data(name, temp, humidity, speed) {
+    $(".card-title").text(name);
+    // $("section").attr("style", "display:inline");
+    var tempF = Math.round((temp - 273.15) * 1.8 + 32);
+    $("#temp2").text('Temperature: ' + tempF + "°");
+    var wind = Math.round(speed);
+    $("#wind2").text('Wind: ' + wind + "mph");
+    $("#humidity2").text('Humidity: ' + humidity + "%");
+  }
 // create el set val text append el to that days card/page(for entire card)
-// 
+
+// function CardData(data) {
+//     for (var i = 0; i < cityWeather.length; i++) {
+//         data.daily[0].weather[0].id
+//         data.daily[1].weather[0].id
+//         data.daily[2].weather[0].id
+//         data.daily[3].weather[0].id
+//         data.daily[4].weather[0].id
+//         data.daily[5].weather[0].id
+//      }
+// }
+
+
+
+// var lookAheadWeath = 'https://api.openweathermap.org/data/2.5/forecast/daily?q='+weathAPI+'&cnt=5&appid=a55cbfd2e3c5718ac038ceb575faa02d'
+// function dailylookaheadAPI(cityWeather) {
+//     fetch(lookAheadWeath)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data)
+//     })
+// }
